af1eb3219bdf5be6b672ba43fd189e219a94a3a0
camilalozanoy <cami321.mcly@gmail.com>
2025-09-10 14:31:00 -0500

    Delete estadisticas.h

--- estadisticas.h
+++
@@ -1,8 +0,0 @@
-#ifndef ESTADISTICAS_H
-#define ESTADISTICAS_H
-
-#include "structs.h"
-
-void calcularEstadisticas(const SalaUCI* salas, int numSalas, const char* idPaciente, double& minV, double& maxV, double& promV, char tipoSensor);
-
-#endif
