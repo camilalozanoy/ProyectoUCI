c3fa97f828f6997f6da66b2f47d9e4065aa07bdf
camilalozanoy <cami321.mcly@gmail.com>
2025-09-10 14:31:27 -0500

    Delete exportarDatos.h

--- exportarDatos.h
+++
@@ -1,8 +0,0 @@
-#ifndef EXPORTAR_DATOS_H
-#define EXPORTAR_DATOS_H
-
-#include "structs.h"
-
-void exportarDatosProcesados(const SalaUCI* salas, int numSalas, const Umbral* configuraciones, int numConfigs);
-
-#endif
