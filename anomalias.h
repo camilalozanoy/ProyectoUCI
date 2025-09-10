d9143fe5ca46d45f533f61674b78f380bdb6f3f7
camilalozanoy <cami321.mcly@gmail.com>
2025-09-10 14:29:50 -0500

    Delete anomalias.h

--- anomalias.h
+++
@@ -1,9 +0,0 @@
-#ifndef ANOMALIAS_H
-#define ANOMALIAS_H
-
-#include "structs.h"
-
-bool detectarAnomalia(const Lectura& lectura,
-                      const Umbral* configuraciones, int numConfigs);
-
-#endif
